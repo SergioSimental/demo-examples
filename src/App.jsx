f47c829ec7e431a2f819489e6b8a19526ac43117
Sergio <sergios3456@gmail.com>
2026-09-04 23:33:17 -0700

    refactor code for reusability removing stats from app component to seperate component

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,69 @@ const Button = (props) => {
   )
 }
 
+const StatisticsLine = (props) => {
+    return (
+        <p>{props.text} {props.value}</p>
+    )
+}
+
 const Statistics = (props) => {
     return (
         <div>
-            <p>good {props.good}</p>
+            <StatisticsLine text="good" value={props.good} />
+            <StatisticsLine text="neutral" value={props.neutral} />
+            <StatisticsLine text="bad" value={props.bad} />
+            <StatisticsLine text="all" value={props.good + props.neutral + props.bad} />
+            <StatisticsLine text="average" value={(props.good - props.bad) / (props.good + props.neutral + props.bad) || 0} />
+            <StatisticsLine text="positive" value={(props.good) / (props.good + props.neutral + props.bad) || 0} />
+        </div>
+    )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    console.log('values before click', good, neutral, bad)
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    console.log('values before click', good, neutral, bad)
+    setNeutral(neutral + 1)
+  }
+
+    const handleBadClick = () => {
+    console.log('values before click', good, neutral, bad)
+    setBad(bad + 1)
+  }
+
+  return (
+    <div> 
+      <h1>give feedback</h1>
+      <Button handleClick={handleGoodClick} text='good' />
+      <Button handleClick={handleNeutralClick} text='neutral' />
+      <Button handleClick={handleBadClick} text='bad' />
+
+    {good + neutral + bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+      <div>
+        <h2>statistics</h2>
+        <Statistics good={good} neutral={neutral} bad={bad} />
+      </div>
+      )}
+    </div>
+  )
+}
+
+export default App
+
+
+{/* const App = () => {
             <p>neutral {props.neutral}</p>
             <p>bad {props.bad}</p>
             <p>all {props.good + props.neutral + props.bad}</p>
@@ -68,4 +127,5 @@ const App = () => {
   )
 }
 
-export default App
+
+export default App */}
