b681c8659397dc403858dafd871577ab553025d7
Sergio <sergios3456@gmail.com>
2026-09-09 17:47:16 -0700

    make the total dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -39,15 +39,18 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+  const total = props.parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
   return (
     <p>
       Number of exercises{' '}
-      {props.parts[0].exercises +
-       props.parts[1].exercises +
-       props.parts[2].exercises}
+      {total}
     </p>
   )
 }
+
 
 const App = () => {
   const course = {
@@ -79,4 +82,4 @@ const App = () => {
   
 }
 
-export default App /**/
+export default App 
