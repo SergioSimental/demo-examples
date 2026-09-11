95b507dac028bec6db1e54ce76eae229d7fffae6
Sergio <sergios3456@gmail.com>
2026-09-11 11:01:21 -0700

    Add a header for adding new name and number

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
-  
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
@@ -54,6 +54,7 @@ const App = () => {
         />
       </div>
 
+      <h2>Add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input 
