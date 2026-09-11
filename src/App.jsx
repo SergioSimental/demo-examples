1cdaddc2a617cea4193f1fc721df831ff7bb1400
Sergio <sergios3456@gmail.com>
2026-09-11 10:59:49 -0700

    Add a filter using a name to display the person

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+  
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
@@ -42,6 +47,13 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       
+      <div>
+        filter shown with: <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
+
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -61,13 +73,16 @@ const App = () => {
       <h2>Numbers</h2>
       
       <div>
-        {persons.map(person =>
+        {persons.
+        filter(person => 
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map(person =>
           <p key={person.name}>
             {person.name} {person.number}
           </p>
         )}
       </div>
-
     </div>
   )
 }
