a643877f8f900000a9c2fbb99de3c8ddb58b5265
Sergio <sergios3456@gmail.com>
2026-09-10 21:19:32 -0700

    refactor number component and add number to phonebook function

--- src/App.jsx
+++ src/App.jsx
@@ -29,34 +29,19 @@ const App = () => {
     }
 
     const personObject = {
-      name: newName
-    }
-
-    setPersons(persons.concat(personObject))
-    setNewName('')
-  }
-
-  const addNumber = (event) => {
-    event.preventDefault()
-  
-    const numberExists = persons.some(person => person.number === newNumber)
-  
-    if (numberExists) {
-      alert(`${newNumber} is already added to phonebook`)
-      return
-    }
-
-    const personObject = {
+      name: newName,
       number: newNumber
     }
 
     setPersons(persons.concat(personObject))
+    setNewName('')
     setNewNumber('')
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      
       <form onSubmit={addPerson}>
         <div>
           name: <input 
@@ -64,15 +49,25 @@ const App = () => {
             onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
+      
       <div>
         {persons.map(person =>
-          <p key={person.name}>{person.name}</p>
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
         )}
       </div>
+
     </div>
   )
 }
