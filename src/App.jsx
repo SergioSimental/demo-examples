612baf88afde74e5ac96b98169bc9108890c9d62
Sergio <sergios3456@gmail.com>
2026-09-10 17:16:15 -0700

    Handle adding persons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,48 @@
 import { useState } from 'react'
 
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>
+        )}
+      </div>
+      <div>debug: {newName}</div>
     </div>
   )
 }
